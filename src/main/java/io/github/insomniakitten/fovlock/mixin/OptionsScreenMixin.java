805d68980f14075f3d8583cb47672651b3941ae6
Chloe Dawn <chloe@sapphic.dev>
2019-02-04 12:37:07 +0000

    Ensure lock widget is only appended for FOV slider

--- src/main/java/io/github/insomniakitten/fovlock/mixin/OptionsScreenMixin.java
+++ src/main/java/io/github/insomniakitten/fovlock/mixin/OptionsScreenMixin.java
@@ -21,6 +21,7 @@ import io.github.insomniakitten.fovlock.gui.widget.FovLockButtonWidget;
 import io.github.insomniakitten.fovlock.mixin.hook.ScreenAccessor;
 import net.minecraft.client.gui.Screen;
 import net.minecraft.client.gui.menu.SettingsScreen;
+import net.minecraft.client.options.GameOptions.Option;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -41,7 +42,15 @@ final class OptionsScreenMixin {
     locals = LocalCapture.CAPTURE_FAILHARD,
     allow = 1
   )
-  private void addFovLockButton(final CallbackInfo ci, final int buttonIndex) {
+  private void addFovLockButton(
+    final CallbackInfo ci,
+    final int buttonIndex,
+    final Option[] options,
+    final int optionsCount,
+    final int optionIndex,
+    final Option option
+  ) {
+    if (Option.FOV != option) return;
     final int offset = FovLock.SLIDER_WIDTH - FovLock.BUTTON_WIDTH;
     final int x = ((Screen) (Object) this).width / 2 - 155 + buttonIndex % 2 * 160 + offset;
     final int y = ((Screen) (Object) this).height / 6 - 12 + 24 * (buttonIndex >> 1);
